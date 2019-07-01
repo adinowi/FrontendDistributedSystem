c48ca8b92214c822fa3256ecc796b78fbc5287af
Adrian Nowicki <adinowi@gmail.com>
2019-07-01 12:21:16 +0200

    sending messages to SQS

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,5 +1,6 @@
 class FileController < ApplicationController
     before_action :set_s3_direct_post
+    skip_before_action :verify_authenticity_token  
 
     def start
         client = Aws::S3::Client.new
@@ -7,8 +8,35 @@ class FileController < ApplicationController
         render :_form
     end
 
+    def send_to_worker
+        sqs = Aws::SQS::Client.new
+        entries = Array.new(params[:keys].size)
+        i=0
+        message_group_id = SecureRandom.uuid
+        params[:keys].each do |key|
+            body = {
+                key: key,
+                operation: params[:operation],
+                value: params[:value]
+            }
+            obj = {
+                id: 'msg' + i.to_s,
+                message_body: JSON[body].to_s,
+                message_group_id: message_group_id,
+                message_deduplication_id: SecureRandom.uuid
+            }
+            entries[i] = obj
+            i += 1
+        end
+        sqs.send_message_batch({
+            queue_url: ENV['SQS_URL'],
+            entries: entries
+            })
+        redirect_to action: "start"
+    end
+
     private
     def set_s3_direct_post
-        @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', signature_expiration: (Time.now.utc + 15.minutes))
+        @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', signature_expiration: (Time.now.utc + 15.minutes), success_action_redirect: request.base_url + "/start")
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'start', to: 'file#start'
+  post 'sendtoworker', to: 'file#send_to_worker'
 end
